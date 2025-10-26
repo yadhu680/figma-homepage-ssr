caa4bbc692226c3d5d1d189a01b12a7c03b0094a
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-10-26 22:26:58 +0530

    improved font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <meta http-equiv="Cache-Control" content="max-age=31536000, public" />
+      <meta http-equiv="Pragma" content="cache" />
+      <link rel="preload" href="/figma-homepage-ssr/fonts/IntegralCF-Regular.otf" as="font" type="font/otf" />
+      <link rel="preload" href="/figma-homepage-ssr/fonts/Satoshi-Regular.woff2" as="font" type="font/woff2" />
       <body>
         {/* Providers sets up MUI theme and caches on client */}
         <Providers>
